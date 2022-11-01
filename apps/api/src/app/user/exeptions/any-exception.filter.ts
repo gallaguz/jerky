a378f724dcfa19492cc93e43fce5e5407423c1a2
gallaguz <gallaguz@gmail.com>
2022-11-02 02:57:50 +0300

    feat: api - add exception filters (for test)

---
+++ apps/api/src/app/user/exeptions/any-exception.filter.ts
@@ -0,0 +1,24 @@
+import {
+    ExceptionFilter,
+    Catch,
+    ArgumentsHost,
+    HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class AnyExceptionFilter implements ExceptionFilter {
+    catch(exception: any, host: ArgumentsHost): void {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+        const status: HttpStatus = exception.status ?? HttpStatus.BAD_REQUEST;
+        const message = exception.message ?? 'Bad request';
+
+        response.status(status).json({
+            statusCode: status,
+            message,
+            timestamp: new Date().toISOString(),
+        });
+    }
+}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,17 +1,14 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { AnyExceptionFilter } from './app/user/exeptions/any-exception.filter';
 
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
     const globalPrefix = 'api';
     app.setGlobalPrefix(globalPrefix);
+    app.useGlobalFilters(new AnyExceptionFilter());
     const port = process.env.PORT || 3333;
     await app.listen(port);
     Logger.log(
