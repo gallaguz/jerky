7b4862306ae01f6241187d0c4fa9db4c7ccea297
gallaguz <gallaguz@gmail.com>
2022-11-02 03:00:23 +0300

    feat: some other

--- apps/user/src/app/controllers/user.events.controller.ts
+++ apps/user/src/app/controllers/user.events.controller.ts
@@ -1,16 +1,12 @@
-import { Body, Controller, Logger } from '@nestjs/common';
-
-import { RMQRoute } from 'nestjs-rmq';
-import { IDomainEvent } from '@jerky/interfaces';
-import { UserEventsService } from '../services/user.events.service';
+import { Controller } from '@nestjs/common';
 
 @Controller('')
 export class UserEventsController {
-    constructor(private readonly userEventsService: UserEventsService) {}
+    constructor() {
+        //
+    }
 
-    // TODO Move this route to alongside microservice with logging
-    @RMQRoute('*.*.log')
-    public async listener(@Body() event: IDomainEvent): Promise<void> {
-        Logger.log(event);
+    public async triggerSomeEvent(): Promise<void> {
+        //
     }
 }
