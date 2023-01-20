e9744bd3ea463218b74be02c47830b60dc535d29
gallaguz <gallaguz@gmail.com>
2023-01-20 05:50:55 +0300

    fix: temporary lock user-api

--- apps/api-microservice/src/app/api/user-api/queries/api.user.queries.controller.ts
+++ apps/api-microservice/src/app/api/user-api/queries/api.user.queries.controller.ts
@@ -1,62 +1,62 @@
-import { ERROR_MESSAGES } from '@jerky/constants';
-import { HttpUserFindFiltered } from '@jerky/contracts';
-import {
-    Body,
-    Controller,
-    Get,
-    HttpCode,
-    NotFoundException,
-    Param,
-    Post,
-    UsePipes,
-    ValidationPipe,
-} from '@nestjs/common';
-
-import { ApiUserQueriesService } from './api.user.queries.service';
-import USER = ERROR_MESSAGES.USER;
-
-@Controller('user')
-export class ApiUserQueriesController {
-    constructor(private readonly userQueriesService: ApiUserQueriesService) {}
-
-    @HttpCode(200)
-    @Get('healthCheck')
-    public async healthCheck(): Promise<UserHealthCheck.Response> {
-        return this.userQueriesService.healthCheck();
-    }
-
-    @UsePipes(new ValidationPipe())
-    @HttpCode(200)
-    @Get(':uuid')
-    public async findOneByUuid(
-        @Param() dto: UserFindByUuid.Request,
-    ): Promise<UserFindByUuid.Response> {
-        const user = await this.userQueriesService.findOneByUuid(dto);
-        if (!user) throw new NotFoundException(USER.NOT_FOUND);
-        return user;
-    }
-
-    // @UseGuards(new JwtGuard([Role.ADMIN]))
-    @UsePipes(new ValidationPipe())
-    @HttpCode(200)
-    @Post('email')
-    public async findOneByEmail(
-        // @JWTPayload() { role }: ITokenPayload,
-        @Body() dto: UserFindByEmail.Request,
-    ): Promise<UserFindByEmail.Response> {
-        const user = await this.userQueriesService.findOneByEmail(dto);
-        if (!user) throw new NotFoundException(USER.NOT_FOUND);
-        return user;
-    }
-
-    @UsePipes(new ValidationPipe())
-    @HttpCode(200)
-    @Post('find')
-    public async findFiltered(
-        @Body() dto: HttpUserFindFiltered.Request,
-    ): Promise<HttpUserFindFiltered.Response> {
-        const users = await this.userQueriesService.findFiltered(dto);
-        if (!users?.length) throw new NotFoundException(USER.NOT_FOUND);
-        return users;
-    }
-}
+// import { ERROR_MESSAGES } from '@jerky/constants';
+// import { HttpUserFindFiltered } from '@jerky/contracts';
+// import {
+//     Body,
+//     Controller,
+//     Get,
+//     HttpCode,
+//     NotFoundException,
+//     Param,
+//     Post,
+//     UsePipes,
+//     ValidationPipe,
+// } from '@nestjs/common';
+//
+// import { ApiUserQueriesService } from './api.user.queries.service';
+// import USER = ERROR_MESSAGES.USER;
+//
+// @Controller('user')
+// export class ApiUserQueriesController {
+//     constructor(private readonly userQueriesService: ApiUserQueriesService) {}
+//
+//     @HttpCode(200)
+//     @Get('healthCheck')
+//     public async healthCheck(): Promise<UserHealthCheck.Response> {
+//         return this.userQueriesService.healthCheck();
+//     }
+//
+//     @UsePipes(new ValidationPipe())
+//     @HttpCode(200)
+//     @Get(':uuid')
+//     public async findOneByUuid(
+//         @Param() dto: UserFindByUuid.Request,
+//     ): Promise<UserFindByUuid.Response> {
+//         const user = await this.userQueriesService.findOneByUuid(dto);
+//         if (!user) throw new NotFoundException(USER.NOT_FOUND);
+//         return user;
+//     }
+//
+//     // @UseGuards(new JwtGuard([Role.ADMIN]))
+//     @UsePipes(new ValidationPipe())
+//     @HttpCode(200)
+//     @Post('email')
+//     public async findOneByEmail(
+//         // @JWTPayload() { role }: ITokenPayload,
+//         @Body() dto: UserFindByEmail.Request,
+//     ): Promise<UserFindByEmail.Response> {
+//         const user = await this.userQueriesService.findOneByEmail(dto);
+//         if (!user) throw new NotFoundException(USER.NOT_FOUND);
+//         return user;
+//     }
+//
+//     @UsePipes(new ValidationPipe())
+//     @HttpCode(200)
+//     @Post('find')
+//     public async findFiltered(
+//         @Body() dto: HttpUserFindFiltered.Request,
+//     ): Promise<HttpUserFindFiltered.Response> {
+//         const users = await this.userQueriesService.findFiltered(dto);
+//         if (!users?.length) throw new NotFoundException(USER.NOT_FOUND);
+//         return users;
+//     }
+// }
--- apps/api-microservice/src/app/api/user-api/queries/api.user.queries.service.ts
+++ apps/api-microservice/src/app/api/user-api/queries/api.user.queries.service.ts
@@ -1,134 +1,132 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { RMQService } from 'nestjs-rmq';
-import {
-    UserFindByEmail,
-    UserFindByUuid,
-    UserFindFiltered,
-    UserHealthCheck,
-    UserValidate,
-} from '@jerky/contracts';
-import { UUUIDService } from '../../common/uuid.service';
-
-@Injectable()
-export class ApiUserQueriesService {
-    constructor(
-        private readonly rmqService: RMQService,
-        private readonly uuidService: UUUIDService,
-    ) {}
-
-    public async healthCheck(): Promise<UserHealthCheck.Response> {
-        return await this.rmqService.send<
-            UserHealthCheck.Request,
-            UserHealthCheck.Response
-        >(UserHealthCheck.topic, <UserHealthCheck.Request>{}, {
-            headers: {
-                requestId: this.uuidService.getUuid(),
-            },
-        });
-    }
-
-    public async validate({
-        email,
-        password,
-    }: UserValidate.Request): Promise<UserValidate.Response | null> {
-        try {
-            return await this.rmqService.send<
-                UserValidate.Request,
-                UserValidate.Response
-            >(
-                UserValidate.topic,
-                { email, password },
-                {
-                    headers: {
-                        requestId: this.uuidService.getUuid(),
-                    },
-                },
-            );
-        } catch (e) {
-            if (e instanceof Error) {
-                Logger.log(e.message);
-            }
-            return null;
-        }
-    }
-
-    public async findOneByUuid({
-        uuid,
-    }: UserFindByUuid.Request): Promise<UserFindByUuid.Response | null> {
-        try {
-            return await this.rmqService.send<
-                UserFindByUuid.Request,
-                UserFindByUuid.Response
-            >(
-                UserFindByUuid.topic,
-                { uuid },
-                {
-                    headers: {
-                        requestId: this.uuidService.getUuid(),
-                    },
-                },
-            );
-        } catch (e) {
-            if (e instanceof Error) {
-                Logger.log(e.message);
-            }
-            return null;
-        }
-    }
-
-    public async findOneByEmail({
-        email,
-    }: UserFindByEmail.Request): Promise<UserFindByEmail.Response | null> {
-        try {
-            return await this.rmqService.send<
-                UserFindByEmail.Request,
-                UserFindByEmail.Response
-            >(
-                UserFindByEmail.topic,
-                { email },
-                {
-                    headers: {
-                        requestId: this.uuidService.getUuid(),
-                    },
-                },
-            );
-        } catch (e) {
-            if (e instanceof Error) {
-                Logger.log(e.message);
-            }
-            return null;
-        }
-    }
-
-    public async findFiltered({
-        take,
-        skip,
-        searchString,
-        orderBy,
-    }: UserFindFiltered.Request): Promise<UserFindFiltered.Response | null> {
-        try {
-            return await this.rmqService.send<
-                UserFindFiltered.Request,
-                UserFindFiltered.Response
-            >(
-                UserFindFiltered.topic,
-                {
-                    take,
-                    skip,
-                    searchString,
-                    orderBy,
-                },
-                {
-                    headers: {
-                        requestId: this.uuidService.getUuid(),
-                    },
-                },
-            );
-        } catch (e) {
-            if (e instanceof Error) {
-                Logger.log(e.message);
-            }
-            return null;
-        }
-    }
-}
+// import { UUIDService } from '@jerky/common';
+// import { InternalUserHealthCheckQueryContract } from '@jerky/contracts';
+// import { Injectable, Logger } from '@nestjs/common';
+// import { RMQService } from 'nestjs-rmq';
+//
+// @Injectable()
+// export class ApiUserQueriesService {
+//     constructor(
+//         private readonly rmqService: RMQService,
+//         private readonly uuidService: UUIDService,
+//     ) {}
+//
+//     public async healthCheck(): Promise<InternalUserHealthCheckQueryContract.Response> {
+//         return await this.rmqService.send<
+//             InternalUserHealthCheckQueryContract.Request,
+//             InternalUserHealthCheckQueryContract.Response
+//         >(
+//             InternalUserHealthCheckQueryContract.topic,
+//             <InternalUserHealthCheckQueryContract.Request>{},
+//             {
+//                 headers: {
+//                     requestId: this.uuidService.getUuid(),
+//                 },
+//             },
+//         );
+//     }
+//
+//     public async validate({
+//         email,
+//         password,
+//     }: UserValidate.Request): Promise<UserValidate.Response | null> {
+//         try {
+//             return await this.rmqService.send<
+//                 UserValidate.Request,
+//                 UserValidate.Response
+//             >(
+//                 UserValidate.topic,
+//                 { email, password },
+//                 {
+//                     headers: {
+//                         requestId: this.uuidService.getUuid(),
+//                     },
+//                 },
+//             );
+//         } catch (e) {
+//             if (e instanceof Error) {
+//                 Logger.log(e.message);
+//             }
+//             return null;
+//         }
+//     }
+//
+//     public async findOneByUuid({
+//         uuid,
+//     }: UserFindByUuid.Request): Promise<UserFindByUuid.Response | null> {
+//         try {
+//             return await this.rmqService.send<
+//                 UserFindByUuid.Request,
+//                 UserFindByUuid.Response
+//             >(
+//                 UserFindByUuid.topic,
+//                 { uuid },
+//                 {
+//                     headers: {
+//                         requestId: this.uuidService.getUuid(),
+//                     },
+//                 },
+//             );
+//         } catch (e) {
+//             if (e instanceof Error) {
+//                 Logger.log(e.message);
+//             }
+//             return null;
+//         }
+//     }
+//
+//     public async findOneByEmail({
+//         email,
+//     }: UserFindByEmail.Request): Promise<UserFindByEmail.Response | null> {
+//         try {
+//             return await this.rmqService.send<
+//                 UserFindByEmail.Request,
+//                 UserFindByEmail.Response
+//             >(
+//                 UserFindByEmail.topic,
+//                 { email },
+//                 {
+//                     headers: {
+//                         requestId: this.uuidService.getUuid(),
+//                     },
+//                 },
+//             );
+//         } catch (e) {
+//             if (e instanceof Error) {
+//                 Logger.log(e.message);
+//             }
+//             return null;
+//         }
+//     }
+//
+//     public async findFiltered({
+//         take,
+//         skip,
+//         searchString,
+//         orderBy,
+//     }: UserFindFiltered.Request): Promise<UserFindFiltered.Response | null> {
+//         try {
+//             return await this.rmqService.send<
+//                 UserFindFiltered.Request,
+//                 UserFindFiltered.Response
+//             >(
+//                 UserFindFiltered.topic,
+//                 {
+//                     take,
+//                     skip,
+//                     searchString,
+//                     orderBy,
+//                 },
+//                 {
+//                     headers: {
+//                         requestId: this.uuidService.getUuid(),
+//                     },
+//                 },
+//             );
+//         } catch (e) {
+//             if (e instanceof Error) {
+//                 Logger.log(e.message);
+//             }
+//             return null;
+//         }
+//     }
+// }
