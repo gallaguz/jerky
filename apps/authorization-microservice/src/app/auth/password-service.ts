76654303037fe64e6ee32d128c2a933a58b490ea
gallaguz <gallaguz@gmail.com>
2023-01-27 14:36:23 +0300

    feat: add sync methods

--- apps/authorization-microservice/src/app/auth/password-service.ts
+++ apps/authorization-microservice/src/app/auth/password-service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { compare, genSalt, hash } from 'bcryptjs';
+import { compare, genSalt, genSaltSync, hash, hashSync } from 'bcryptjs';
 
 @Injectable()
 export class PasswordService {
-    constructor(private readonly configService: ConfigService) {}
+    // constructor() {} // private readonly configService: ConfigService
 
     public async hashPassword(
         password: string,
     ): Promise<{ salt: string; passwordHash: string }> {
         const salt = await this.generateSalt();
         const passwordHash = await hash(password, salt);
+        return { salt, passwordHash };
+    }
+    public hashPasswordSync(password: string): {
+        salt: string;
+        passwordHash: string;
+    } {
+        const salt = this.generateSaltSync();
+        const passwordHash = hashSync(password, salt);
         return { salt, passwordHash };
     }
     public async validatePassword(
@@ -21,5 +28,8 @@ export class PasswordService {
     }
     private async generateSalt(rounds = 12): Promise<string> {
         return await genSalt(rounds);
+    }
+    private generateSaltSync(rounds = 12): string {
+        return genSaltSync(rounds);
     }
 }
