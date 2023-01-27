5a0ab193734fb3accec1de43b9271823e1689772
gallaguz <gallaguz@gmail.com>
2023-01-27 14:35:33 +0300

    feat: rename file uuid.service.ts to uuid-service.ts

--- libs/common/src/lib/index.ts
+++ libs/common/src/lib/index.ts
@@ -2,4 +2,4 @@ export * from './contracts-validation.service';
 export * from './generate';
 export * from './jwt-validator';
 export * from './random';
-export * from './uuid.service';
+export * from './uuid-service';
